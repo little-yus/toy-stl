0f9a65c520d974af70b8f7f4260cb1defa37ef68
little-yus <159779618+little-yus@users.noreply.github.com>
2024-02-12 20:52:39 +0200

    Constucting vector of given size

--- src/my_std/vector.hpp
+++ src/my_std/vector.hpp
@@ -7,14 +7,37 @@ namespace my
     class vector
     {
     public:
+        vector();
+        vector(std::size_t size);
+
         bool empty();
+        std::size_t size() const;
     private:
+        std::size_t size_ {0};
     };
+
+    template <typename T>
+    vector<T>::vector()
+    {
+
+    }
+
+    template <typename T>
+    vector<T>::vector(std::size_t size) : size_{size}
+    {
+
+    }
 
     template <typename T>
     bool vector<T>::empty()
     {
         return true;
+    }
+
+    template <typename T>
+    std::size_t vector<T>::size() const
+    {
+        return size_;
     }
 }
 
--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -1,8 +1,22 @@
 #include "doctest/doctest.h"
 #include "my_std/vector.hpp"
 
-TEST_CASE("Default construction") {
+TEST_CASE("Default constructor") {
     my::vector<int> vec;
     
     CHECK(vec.empty());
+}
+
+TEST_CASE("Constructor from size") {
+    SUBCASE("Zero size") {
+        my::vector<int> vec(0);
+    
+        CHECK(vec.size() == 0);
+    }
+    
+    SUBCASE("Nonzero size") {
+        my::vector<int> vec(123);
+    
+        CHECK(vec.size() == 123);
+    }
 }
