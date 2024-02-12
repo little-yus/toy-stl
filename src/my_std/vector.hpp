36f306f7539180f2c91554d54c366b8ee4d9f210
little-yus <159779618+little-yus@users.noreply.github.com>
2024-02-12 18:10:13 +0200

    Project setup

---
+++ src/my_std/vector.hpp
@@ -0,0 +1,14 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+namespace my
+{
+    template <typename T>
+    class vector
+    {
+
+    };
+}
+
+
+#endif /* VECTOR_HPP */
---
+++ tests/vector/vector.cpp
@@ -0,0 +1,7 @@
+#include "doctest/doctest.h"
+#include "my_std/vector.hpp"
+
+TEST_CASE("testing the factorial function") {
+    my::vector<int> test;
+    CHECK(sizeof(test) == 1);
+}
