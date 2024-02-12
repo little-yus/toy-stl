798e3fb5ab289a11a8291d36312c9e5b49ae9ae3
little-yus <159779618+little-yus@users.noreply.github.com>
2024-02-12 20:02:49 +0200

    Empty vector check

--- src/my_std/vector.hpp
+++ src/my_std/vector.hpp
@@ -6,8 +6,16 @@ namespace my
     template <typename T>
     class vector
     {
-
+    public:
+        bool empty();
+    private:
     };
+
+    template <typename T>
+    bool vector<T>::empty()
+    {
+        return true;
+    }
 }
 
 
--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -1,7 +1,8 @@
 #include "doctest/doctest.h"
 #include "my_std/vector.hpp"
 
-TEST_CASE("testing the factorial function") {
-    my::vector<int> test;
-    CHECK(sizeof(test) == 1);
+TEST_CASE("Default construction") {
+    my::vector<int> vec;
+    
+    CHECK(vec.empty());
 }
