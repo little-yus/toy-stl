99644611db6efb74558c460e4cfc615cb9309b40
little-yus <159779618+little-yus@users.noreply.github.com>
2024-02-14 19:15:12 +0200

    Heap memory allocation for vector data

--- src/my_std/vector.hpp
+++ src/my_std/vector.hpp
@@ -1,6 +1,8 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include <memory>
+
 namespace my
 {
     template <typename T>
@@ -17,9 +19,10 @@ namespace my
         const T& operator[] (std::size_t index) const;
     private:
         std::size_t size_ {0};
-        T data {0}; // This is not enough now
+        std::unique_ptr<T[]> data_ {nullptr};
     };
 
+    // Constructors
     template <typename T>
     vector<T>::vector()
     {
@@ -27,17 +30,26 @@ namespace my
     }
 
     template <typename T>
-    vector<T>::vector(std::size_t size) : size_{size}
+    vector<T>::vector(std::size_t size) : size_{size}, data_{std::make_unique<T[]>(size)}
     {
 
     }
 
+    // Note:
+    // When you use new[] each element is initialized by the default constructor except when the type is a built-in type. 
+    // Built-in types are left unitialized by default.
     template <typename T>
-    vector<T>::vector(std::size_t size, const T& value) : size_{size}, data{value}
+    vector<T>::vector(std::size_t size, const T& value) : size_{size}, data_{std::make_unique<T[]>(size)}
     {
+        T* begin = data_.get();
+        T* end = data_.get() + size;
 
+        for (T* ptr = begin; ptr != end; ptr += 1) {
+            *ptr = value;
+        }
     }
 
+    // Other functions
     template <typename T>
     bool vector<T>::empty()
     {
@@ -53,13 +65,13 @@ namespace my
     template <typename T>
     T& vector<T>::operator[] (std::size_t index)
     {
-        return data;
+        return data_[index];
     }
 
     template <typename T>
     const T& vector<T>::operator[] (std::size_t index) const
     {
-        return data;
+        return data_[index];
     }
 }
 
