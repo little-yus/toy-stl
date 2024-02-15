7c6811d79949fd6c6f1b468af6248aa58ecb4661
little-yus <159779618+little-yus@users.noreply.github.com>
2024-02-15 20:32:51 +0200

    Memory allocation reimplemented to use ::operator new and placement new, to avoid reallocating on every push_back or pop_back

--- src/my_std/vector.hpp
+++ src/my_std/vector.hpp
@@ -1,8 +1,8 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-#include <memory>
 #include <stdexcept>
+#include <new>
 
 namespace my
 {
@@ -23,12 +23,17 @@ namespace my
         vector(const vector& other);
         vector& operator= (vector other);
 
-        vector(vector&& other) = default;
-        vector& operator= (vector&& other) = default;
+        vector(vector&& other);
+        vector& operator= (vector&& other);
+
+        ~vector();
+
+        void swap(vector& other);
 
         bool empty();
         std::size_t size() const;
 
+        // Accessors
         T& operator[] (std::size_t index);
         const T& operator[] (std::size_t index) const;
 
@@ -41,9 +46,15 @@ namespace my
         T& back();
         const T& back() const;
 
+        // Modifiers
+        void push_back(const T& value);
+        void push_back(T&& value);
+        void pop_back();
+
     private:
         std::size_t size_ {0};
-        std::unique_ptr<T[]> data_ {nullptr};
+        std::size_t capacity_ {0};
+        T* data_ {nullptr};
     };
 
     // Constructors
@@ -54,38 +65,93 @@ namespace my
     }
 
     template <typename T>
-    vector<T>::vector(std::size_t size) : size_{size}, data_{std::make_unique<T[]>(size)}
+    vector<T>::vector(std::size_t size) :
+        size_{size},
+        capacity_{size},
+        data_{static_cast<T*>(::operator new (sizeof(T) * size))}
     {
+        T* begin = data_;
+        T* end = data_ + size;
 
+        for (T* ptr = begin; ptr != end; ptr += 1) {
+            new (ptr) T();
+        }
     }
 
     // Note:
     // When you use new[] each element is initialized by the default constructor except when the type is a built-in type. 
     // Built-in types are left unitialized by default.
     template <typename T>
-    vector<T>::vector(std::size_t size, const T& value) : size_{size}, data_{std::make_unique<T[]>(size)}
+    vector<T>::vector(std::size_t size, const T& value) :
+        size_{size},
+        capacity_{size},
+        data_{static_cast<T*>(::operator new (sizeof(T) * size))}
     {
-        T* begin = data_.get();
-        T* end = data_.get() + size;
+        T* begin = data_;
+        T* end = data_ + size;
 
         for (T* ptr = begin; ptr != end; ptr += 1) {
-            *ptr = value;
+            new (ptr) T(value);
         }
     }
 
     template <typename T>
-    vector<T>::vector(const vector& other) : size_{other.size_}, data_{std::make_unique<T[]>(other.size_)}
+    vector<T>::vector(const vector& other) :
+        size_{other.size_},
+        capacity_{other.size_}, // vector does not have to copy capacity
+        data_{static_cast<T*>(::operator new (sizeof(T) * other.size_))}
     {
         for (std::size_t i = 0; i < size_; i += 1) {
-            data_[i] = other.data_[i];
+            new (data_ + i) T(other.data_[i]);
         }
     }
 
+    // Maybe reimplement for better performance?
     template <typename T>
     vector<T>& vector<T>::operator= (vector other)
     {
-        std::swap(*this, other);
+        this->swap(other);
         return *this;
+    }
+
+    template <typename T>
+    vector<T>::vector(vector&& other) :
+        size_{other.size_},
+        capacity_{other.capacity_},
+        data_{other.data_}
+    {
+        other.size_ = 0;
+        other.capacity_ = 0;
+        other.data_ = nullptr;
+    }
+
+    template <typename T>
+    vector<T>& vector<T>::operator= (vector&& other)
+    {
+        this->swap(vector<T>(std::move(other)));
+        return *this;
+    }
+
+    template <typename T>
+    vector<T>::~vector()
+    {
+        T* begin = data_;
+        T* end = data_ + size_;
+
+        for (T* ptr = begin; ptr != end; ptr += 1) {
+            ptr->~T();
+        }
+
+        ::operator delete(static_cast<void*>(data_)); // Not sure if this is ok
+    }
+
+    template <typename T>
+    void vector<T>::swap(vector& other)
+    {
+        using std::swap;
+        swap(this->size_, other.size_);
+        swap(this->capacity_, other.capacity_);
+        swap(this->data_, other.data_);
     }
 
     // Other functions
@@ -155,6 +221,24 @@ namespace my
     const T& vector<T>::back() const
     {
         return (*this)[size() - 1];
+    }
+
+    template <typename T>
+    void vector<T>::push_back(const T& value)
+    {
+
+    }
+
+    template <typename T>
+    void vector<T>::push_back(T&& value)
+    {
+
+    }
+
+    template <typename T>
+    void vector<T>::pop_back()
+    {
+
     }
 }
 
